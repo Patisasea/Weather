d40f9e6bcc33e3d53cdfc3c7f5832d551ff9874a
Pat Seaman <patisasea@gmail.com>
2022-02-04 20:22:22 -0800

    added working weather fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var lat;
-var long;
+var lon;
 var city;
 var weather;
+var cityLocation;
 
 
 $(".button").click(function () {
@@ -14,20 +15,35 @@ $(".button").click(function () {
     pullCity();
 })
 
-function pullCity() {
+async function pullCity() {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=b09c26db1fd4f75c2f90565dbd6fb00b`)
         .then(function (response) {
             return response.json()
         }).then(function (data) {
-            cityLocation(data)
-            console.log(data);
+            cityLocation = data[0];
+            console.log(cityLocation);
+        }).then(function(){
+            cityCords();
+        }).then(function(){
+            forcast();
         }).catch(function (error) {
 
         })
+    
 }
 
-var cityLocation = function (data) {
-    lat = data.child().lat;
-    long = data.child().lon;
-    console.log(lat + ' ' + long);
+function cityCords(){
+    lat = cityLocation.lat;
+    lon = cityLocation.lon;
+    console.log(lat + " " + lon)
+}
+
+function forcast(){
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=b09c26db1fd4f75c2f90565dbd6fb00b`)
+    .then(function(response){
+        return response.json();
+    }).then(function(data){
+        weather = data;
+        console.log(weather);
+    })
 }
